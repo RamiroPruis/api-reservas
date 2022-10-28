a69b754bc9fb1ca33c5e73cc1011eba78f4ecccd
JoaquinFioriti <joaquinfioritifreelance@gmail.com>
2022-10-28 17:34:44 -0300

    mas cositas ricas

--- index.js
+++ index.js
@@ -8,20 +8,17 @@ export const ERROR_CODE = 404
 
 const server = http.createServer((req,res)=>{
 
-    const queryObject = url.parse(req.url, true).query;
-    console.log(queryObject);
-
-    // if (!url.includes('/api/reserva')){
-    //     errorHandler(400,"Endpoint no valido",res)
-    // } else{
+    if (!url.includes('/api/reserva')){
+        errorHandler(400,"Endpoint no valido",res)
+    } else{
         
-    //     if(url.match(/api\/reserva(\/[0-9]+)?/)){ //Como hacemos con los query params? ashe 
-    //         reservas(req,res)
-    //     }else{
-    //         errorHandler(400,"Endpoint no valido",res)
-    //     }    
-    //     console.log("se ve esto?")
-    // }
+        if(url.match(/api\/reserva(\/[0-9]+)?/)){ //Como hacemos con los query params? ashe 
+            reservas(req,res)
+        }else{
+            errorHandler(400,"Endpoint no valido",res)
+        }    
+        console.log("se ve esto?")
+    }
 })
 
 server.listen(PORT,()=>{
